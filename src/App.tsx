103f655ae72fa6df25a89296720dce0c42507913
arhammusheer <arhammusheer@gmail.com>
2023-08-28 17:45:01 -0400

    Bussy update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,277 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Box,
+  Card,
+  Grid,
+  Heading,
+  Progress,
+  Spacer,
+  Stack,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import "./App.css";
+import "@fontsource/work-sans";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box
+      h="100vh"
+      w="100vw"
+      bg={useColorModeValue("gray.50", "gray.900")}
+      p={4}
+    >
+      <Grid
+        templateAreas={`
+        "name name name name"        
+        "bus bus bus bus"
+        "time time time time"
+        `}
+        h="100%"
+        gap={4}
+      >
+        <Stack direction={"row"} gridArea="name">
+          <Name />
+          <Spacer />
+          <DateTime />
+        </Stack>
+        <Card gridArea="bus">
+          <Bus />
+        </Card>
+        <Card gridArea="time"></Card>
+      </Grid>
+    </Box>
+  );
 }
 
-export default App
+const Name = () => {
+  const Greeting = () => {
+    const date = new Date();
+    const hour = date.getHours();
+    if (hour < 12) {
+      return "Morning";
+    } else if (hour < 18) {
+      return "Afternoon";
+    } else {
+      return "Evening";
+    }
+  };
+
+  return (
+    <Stack direction="column" spacing={4} p={6} justify={"center"}>
+      <Heading size="lg" fontFamily={"'Work Sans', sans-serif"}>
+        Hey There Arham
+      </Heading>
+      <Heading size="3xl">Good {Greeting()}</Heading>
+    </Stack>
+  );
+};
+
+const DateTime = () => {
+  const [date, setDate] = useState(new Date());
+  const day = date.getDay(); // 0 - 6
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  // Rerender every second
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Stack
+      direction="column"
+      spacing={4}
+      p={8}
+      justify={"center"}
+      align={"end"}
+    >
+      <Heading size="lg" fontFamily={"'Work Sans', sans-serif"}>
+        Today is {days[day]}, {date.toLocaleDateString()}
+      </Heading>
+
+      <Heading size="3xl">{date.toLocaleTimeString()}</Heading>
+    </Stack>
+  );
+};
+
+const Bus = () => {
+  const [bus, setBus] = useState<typeof EXAMPLE_BUS | null>(null);
+  const [busTime, setBusTime] = useState(0); // In epoch seconds
+  const [busTimeNext, setBusTimeNext] = useState(0); // In epoch seconds, -1 if no next bus
+
+  // Update bus time
+  useEffect(() => {
+    if (bus) {
+      const now = Date.now() / 1000;
+      const nextBus =
+        bus.route_departures[0].itineraries[0].schedule_items[0].departure_time;
+      setBusTime(Math.round((nextBus - now) / 60));
+
+      if (
+        bus.route_departures[0].itineraries[0].schedule_items.length > 1 &&
+        bus.route_departures[0].itineraries[0].schedule_items[1]
+      ) {
+        const nextBusNext =
+          bus.route_departures[0].itineraries[0].schedule_items[1]
+            .departure_time;
+        setBusTimeNext(Math.round((nextBusNext - now) / 60));
+      } else {
+        setBusTimeNext(-1);
+      }
+    }
+  }, [bus]);
+
+  const fetchBus = () => {
+    axios.get("https://home-apis.croissant.one/bus").then((res) => {
+      setBus(res.data);
+    });
+  };
+
+  // Fetch bus data
+  useEffect(() => {
+    fetchBus();
+    const interval = setInterval(() => {
+      fetchBus();
+    }, 1000 * 60 * 10); // every 10 minutes
+    return () => clearInterval(interval);
+  }, []);
+
+  // refresh bus time every minute
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (bus) {
+        const now = Date.now() / 1000;
+        const nextBus =
+          bus.route_departures[0].itineraries[0].schedule_items[0]
+            .departure_time;
+        setBusTime(Math.floor((nextBus - now) / 60));
+      }
+    }, 60000);
+    return () => clearInterval(interval);
+  }, [bus]);
+
+  return (
+    <Stack
+      direction="column"
+      spacing={4}
+      p={6}
+      justify={"center"}
+      bg={useColorModeValue("pink.200", "pink.600")}
+      color={useColorModeValue("black", "white")}
+      borderRadius={8}
+    >
+      <Stack direction="row" spacing={4}>
+        <Box>
+          <Heading size="lg" fontFamily={"'Work Sans', sans-serif"}>
+            Bus Departs In
+          </Heading>
+          <Heading size="3xl">
+            {Math.floor(busTime / 60) > 0 &&
+              Math.floor(busTime / 60) + " Hours "}
+            {busTime % 60} minutes
+          </Heading>
+
+          <Heading size="sm">
+            {bus?.route_departures[0].itineraries[0].headsign}
+          </Heading>
+        </Box>
+        <Spacer />
+        <Box textAlign="right">
+          <Heading size="sm" fontFamily={"'Work Sans', sans-serif"}>
+            Next Bus Departs In
+          </Heading>
+          <Heading size="xl">
+            {busTimeNext > -1
+              ? (Math.floor(busTimeNext / 60) > 0
+                  ? Math.floor(busTimeNext / 60) + " Hours "
+                  : "") +
+                (busTimeNext % 60) +
+                " Minutes"
+              : "No more buses today"}
+          </Heading>
+          <Heading size="sm">
+            {bus?.route_departures[0].itineraries[0].headsign}
+          </Heading>
+        </Box>
+      </Stack>
+      <Progress
+        value={30 - busTime}
+        max={30}
+        colorScheme="pink"
+        size="lg"
+        borderRadius={8}
+      />
+    </Stack>
+  );
+};
+
+export default App;
+
+const EXAMPLE_BUS = {
+  route_departures: [
+    {
+      global_route_id: "PVTAMA:116200",
+      itineraries: [
+        {
+          branch_code: "",
+          direction_headsign: "Sunderland",
+          direction_id: 0,
+          headsign: "Sunderland",
+          merged_headsign: "Sunderland",
+          schedule_items: [
+            {
+              departure_time: 1693255980,
+              is_cancelled: false,
+              is_real_time: false,
+              rt_trip_id: "4017495-UM2223-UMTS-Weekday-78",
+              scheduled_departure_time: 1693255980,
+              trip_search_key: "PVTAMA:43094228:5:0:16",
+              wheelchair_accessible: 1,
+            },
+            {
+              departure_time: 1693258080,
+              is_cancelled: false,
+              is_real_time: false,
+              rt_trip_id: "4017523-UM2223-UMTS-Weekday-78",
+              scheduled_departure_time: 1693258080,
+              trip_search_key: "PVTAMA:43094228:5:0:17",
+              wheelchair_accessible: 1,
+            },
+            {
+              departure_time: 1693260180,
+              is_cancelled: false,
+              is_real_time: false,
+              rt_trip_id: "4017496-UM2223-UMTS-Weekday-78",
+              scheduled_departure_time: 1693260180,
+              trip_search_key: "PVTAMA:43094228:5:0:18",
+              wheelchair_accessible: 1,
+            },
+          ],
+        },
+      ],
+      mode_name: "Bus",
+      real_time_route_id: "20031",
+      route_color: "ea6083",
+      route_long_name: "Sunderland  /  South Amherst",
+      route_network_id: "PVTA|Boston",
+      route_network_name: "PVTA",
+      route_short_name: "31",
+      route_text_color: "ffffff",
+      route_type: 3,
+      sorting_key: "31",
+      tts_long_name: "Sunderland  /  South Amherst",
+      tts_short_name: "31",
+    },
+  ],
+};
